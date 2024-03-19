b13ea10322ac0a861a42c076a7a61f1adb290277
Stef40IT <Stef40toBG@gmail.com>
2024-03-19 13:49:32 +0200

    Added a Data Folder and DbContext

---
+++ ExpensesTracker/ExpensesTracker.Data/AplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesTracker.Data
+{
+    public class AplicationDbContext:DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
