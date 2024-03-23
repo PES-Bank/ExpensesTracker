b83c7cb1101d0920186df37542b393e7cde4dc15
enis000-hub <eniskasper32@gmail.com>
2024-03-23 22:27:41 +0200

    Created Remove View. Added Add and Remove Controllers in ExpensesController

--- ExpensesTracker.Web/ExpensesTracker.Web/Controllers/ExpensesController.cs
+++ ExpensesTracker.Web/ExpensesTracker.Web/Controllers/ExpensesController.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpensesTracker.Web.Data;
+using ExpensesTracker.Web.Data.Entities;
+using ExpensesTracker.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExpensesTracker.Web.Controllers
 {
     public class ExpensesController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+        public ExpensesController(ApplicationDbContext dbContext) 
+        { 
+            this.dbContext=dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddExpenseViewModel viewModel )
+        {
+            var expense = new Expense
+            {
+                ExpenseName = viewModel.ExpenseName,
+                ExpenseDescription = viewModel.ExpenseDescription,
+                ExpenseType = viewModel.ExpenseType,
+                ExpenseAmount = viewModel.ExpenseAmount,
+            };
+            await dbContext.Expenses.AddAsync(expense);
+            await dbContext.SaveChangesAsync();
+            return View();
+        }
+       
     }
 }
